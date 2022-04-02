4ad0ec62a82058046d07ff7038ce0571613c0d8b
jamel123a <issaouijamel7@gmail.com>
2022-04-02 02:05:11 +0100

    Enter

---
+++ frondend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, world React</h1>
+        <p>jamel issaoui</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
